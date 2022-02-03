39e0eff192eafafc36e918fbd0ae60dfcd27b754
= <n01437978@unf.edu>
2022-02-03 00:18:15 -0500

    added font awesome

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { faTrashAlt, faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons"
 import App from './App.vue'
 import './index.css'
 
-createApp(App).use(createPinia).mount('#app')
+library.add(faTrashAlt, faArrowUp, faArrowDown)
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .use(createPinia)
+    .mount('#app')
