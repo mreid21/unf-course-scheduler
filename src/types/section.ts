d41acb8b4a2a5f943df9df5bdeb84b3e1fe2db1c
= <n01437978@unf.edu>
2022-02-13 20:17:54 -0500

    updated section types

--- src/types/section.ts
+++ src/types/section.ts
@@ -1,6 +1,6 @@
-export type SectionField = keyof Section;
+type SectionField = keyof Section;
 
-export interface Section {
+interface Section {
   section_id: number;
   course_code: string;
   credit_hours: number;
@@ -9,4 +9,20 @@ export interface Section {
   building_number?: string;
   building_name?: string;
   campus_name: string;
+}
+
+type SectionBuilder = {
+  course_id: number,
+  instructor_id: number,
+  room_id?: number,
+  campus_id: number,
+  slot_id: number,
+  building_id?: number
+}
+
+
+export {
+  SectionField,
+  Section,
+  SectionBuilder
 }
