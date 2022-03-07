0084fb214da5a047b3de8154c22912b60e560de7
= <n01437978@unf.edu>
2022-03-07 11:53:00 -0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/unf-course-scheduler',
+  base: '/unf-course-scheduler/',
   plugins: [vue()],
 });
