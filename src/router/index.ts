5d7a834732c8b06c6dd63354795473e5331b292b
= <n01437978@unf.edu>
2022-04-04 12:17:33 -0400

    quick fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'Base',
+      component: () => import('../views/Login.vue')
+    },
+    {
       path: '/Home',
       name: 'Home',
       component: () => import('../views/Home.vue'),
